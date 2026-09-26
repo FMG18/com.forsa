bf57b12df9b2de0b52452a46ec3dd66f57954187
FMG18 <48799138+FMG18@users.noreply.github.com>
2026-09-26 03:00:47 +0300

    fix: import Compose layout direction

--- app/src/main/java/com/forsa/app/MainActivity.kt
+++ app/src/main/java/com/forsa/app/MainActivity.kt
@@ -46,6 +46,7 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.LayoutDirection
+import androidx.compose.ui.unit.LocalLayoutDirection
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.credentials.ClearCredentialStateRequest
